6d057c1015709bb6017b8668d5a036f8137bc937
Vikas Singh <shyvikas63@gmail.com>
2024-09-30 23:11:03 +0530

    app.js minor change

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(cors());
 //app.set("view engine", "ejs")
 
 app.get('/', (req, res) => {
-    res.send("TODO-MERN PRODUCTION SERVER IS RUNNING!!!")  
+    res.send("TODO-MERN PRODUCTION SERVER IS UP AND RUNNING")  
     //res.sendFile(path.join(__dirname + "/Home.html"))
 })
 
