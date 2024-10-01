6c2bb60e64ab7918a8fb376e8fa0229bcfb2db83
Vikas Singh <shyvikas63@gmail.com>
2024-10-01 14:23:42 +0530

    terraform updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
         access_key = "${var.access_key}"
         secret_key = "${var.secret_key}"
-        region = "ap-south-1"
+        region = "${var.region}"
 }
 
 resource "aws_instance" "ec2_instance" { # ec2_instance - this is just a local variable
@@ -10,30 +10,54 @@ resource "aws_instance" "ec2_instance" { # ec2_instance - this is just a local v
         subnet_id = "${var.subnet_id}"
         instance_type = "${var.instance_type}"
         key_name = "${var.ami_key_pair_name}"
+        vpc_security_group_ids = [aws_security_group.sg.id] # Replace with your security group ID
 
-        vpc_security_group_ids = ["sg-04dcb1e5a6e5e5b1c"] # Replace with your security group ID
-
+        root_block_device {
+                volume_type = "${var.volume_type}"
+                volume_size = "${var.volume_size}"
+        }
 
         tags = {
-                Name = "todo-app-project"
+                Name = "${var.instance_name}-${count.index}"
         }
 }
 
-resource "aws_security_group" "todo-sg" { # terraform-demo-sg - This is just a local variable
-        name = "todo-app-sg"
-        description = "Example security group for EC2 instance"
-
-        ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_security_group" "sg" { # demo - This is just a local variable
+        name = "${var.aws_security_group_name}"
+        description = "security group for EC2 instance"
+}
+resource "aws_security_group_rule" "ingress_ssh" {
+        security_group_id = aws_security_group.sg.id
+        type = "ingress"
+        from_port   = "${var.ingress_rules[0].from_port}"
+        to_port     = "${var.ingress_rules[0].to_port}"
+        protocol    = "${var.ingress_rules[0].protocol}"
+        cidr_blocks = "${var.ingress_rules[0].cidr_blocks}"
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_security_group_rule" "ingress_http" {
+        security_group_id = aws_security_group.sg.id
+        type = "ingress"
+        from_port   = "${var.ingress_rules[1].from_port}"
+        to_port     = "${var.ingress_rules[1].to_port}"
+        protocol    = "${var.ingress_rules[1].protocol}"
+        cidr_blocks = "${var.ingress_rules[1].cidr_blocks}"
   }
+
+resource "aws_security_group_rule" "ingress_https" {
+        security_group_id = aws_security_group.sg.id
+        type = "ingress"
+        from_port   = "${var.ingress_rules[2].from_port}"
+        to_port     = "${var.ingress_rules[2].to_port}"
+        protocol    = "${var.ingress_rules[2].protocol}"
+        cidr_blocks = "${var.ingress_rules[2].cidr_blocks}"
+  }
+
+resource "aws_security_group_rule" "egress" {
+        security_group_id = aws_security_group.sg.id
+        type = "egress"
+        from_port   = "${var.egress_rules[0].from_port}"
+        to_port     = "${var.egress_rules[0].to_port}"
+        protocol    = "${var.egress_rules[0].protocol}"
+        cidr_blocks = "${var.egress_rules[0].cidr_blocks}"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,30 +10,91 @@ variable "secret_key" {
         type = string
 }
 
+variable "region" {
+        description = "instance region"
+        type = string
+  
+}
+
+variable "availability_zone" {
+        description = "availability zone"
+        type = string  
+}
+
+variable "vpc_id" {
+        description = "The VPC the instance(s) will be created in"
+        type = string
+  
+}
+
 variable "instance_name" {
-        description = "name of the instance to be created"
-        default = "awsbuilder-demo"
+        description = "name of the instance"
+        type = string
+        default = " "
 }
 
 variable "instance_type" {
-        default = "t2.micro"
+        description = "Instance type"
+        type = string
+}
+
+variable "volume_type" {
+        description = "volume type"
+        type = string
+  
+}
+
+variable "volume_size" {
+        description = "volume size"
+        type = number
+  
 }
 
 variable "subnet_id" {
         description = "The VPC subnet the instance(s) will be created in"
-        default = "subnet-0c95a09a445226173"
+        type = string
 }
 
 variable "ami_id" {
-        description = "The AMI to be used"
-        default = "ami-08e5424edfe926b43"
+        description = "ID of the AMI to use for the instance"
+        type = string
 }
 
 variable "number_of_instances"{
         description = "number of instances to be created"
-        default = 1
+        type = number
 }
 
+
 variable "ami_key_pair_name"{
-        default = "AWServer3-key"
+        description = "value of the key pair name"
+        type = string
+}
+
+variable "aws_security_group_name" {
+        description = "security group name"
+        type = string
+  
+}
+
+variable "ingress_rules" {
+  description = "List of ingress rules"
+  type        = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = []
+}
+
+variable "egress_rules" {
+  description = "List of egress rules"
+  type        = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = []
 }
